de391888fccf7e5dba1b41842232d2ba8b0fcb51
Erick Garcia <erickgegarcia@gmail.com>
2024-06-11 23:01:52 -0300

    adicionando tsup para build do projeto

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 app
   .listen({
     port: 3333,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server Running!')
