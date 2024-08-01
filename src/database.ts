9fde615554b516a099936da7c4b9b3f47686ee57
Erick Garcia <82339007+erickggarcia@users.noreply.github.com>
2024-08-01 10:31:22 -0300

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -8,15 +8,15 @@ if (!process.env.DATABASE_URL) {
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
   connection:
-    env.DATABASE_CLIENT !== 'sqlite'
-      ? env.DATABASE_URL
-      : {
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
           filename: env.DATABASE_URL,
-        },
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './src/db/migrations',
+    directory: './db/migrations',
   },
 }
 
